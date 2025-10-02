15e8598859bf31a8125a989aad86368357c17192
mdselimme <mdselimdev@gmail.com>
2025-10-03 02:45:01 +0600

    add: footer section

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from "@/components/footer-04/footer-04";
 import Navbar from "@/components/navbar-04/Navbar";
 import React from "react";
 
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      {children}
+      <div className="grow-1">{children}</div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer-04/footer-04.tsx
@@ -0,0 +1,101 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Github, LinkedinIcon, XIcon } from "lucide-react";
+import Link from "next/link";
+import { Logo } from "../navbar-04/logo";
+
+const footerLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Blogs",
+    href: "/blogs",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Login",
+    href: "/login",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="grow bg-muted" />
+      <footer className="border-t">
+        <div className="max-w-(--breakpoint-xl) mx-auto">
+          <div className="py-12 flex flex-col sm:flex-row items-start justify-between gap-x-8 gap-y-10 px-6 xl:px-0">
+            <div>
+              {/* Logo */}
+              <Logo />
+
+              <ul className="mt-6 flex items-center gap-4 flex-wrap">
+                {footerLinks.map(({ title, href }) => (
+                  <li key={title}>
+                    <Link
+                      href={href}
+                      className="text-muted-foreground hover:text-foreground"
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Subscribe Newsletter */}
+            <div className="max-w-xs w-full">
+              <h6 className="font-medium">Stay up to date</h6>
+              <form className="mt-6 flex items-center gap-2">
+                <Input type="email" placeholder="Enter your email" />
+                <Button>Subscribe</Button>
+              </form>
+            </div>
+          </div>
+          <Separator />
+          <div className="py-8 flex flex-col-reverse sm:flex-row items-center justify-between gap-x-2 gap-y-5 px-6 xl:px-0">
+            {/* Copyright */}
+            <span className="text-muted-foreground">
+              &copy; {new Date().getFullYear()}{" "}
+              <Link href="/" target="_blank">
+                Selim Dev
+              </Link>
+              . All rights reserved.
+            </span>
+
+            <div className="flex items-center gap-5 text-muted-foreground">
+              <Link
+                href="https://www.linkedin.com/in/mdselimme"
+                target="_blank"
+              >
+                <LinkedinIcon className="h-5 w-5" />
+              </Link>
+              <Link href="https://x.com/mdselimme" target="_blank">
+                <XIcon className="h-5 w-5" />
+              </Link>
+              <Link href="https://github.com/mdselimme" target="_blank">
+                <Github className="h-5 w-5" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
