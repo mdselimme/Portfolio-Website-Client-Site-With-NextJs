5d4969a1e933ab4f934f589cb209f8f58f8cc5ef
mdselimme <mdselimdev@gmail.com>
2025-10-05 04:34:02 +0600

    add: add project page

--- src/app/(dashboard)/dashboard/add-project/page.tsx
+++ src/app/(dashboard)/dashboard/add-project/page.tsx
@@ -1,9 +1,21 @@
+import AddProjectForm from "@/components/modules/Project/AddProjectForm";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import React from "react";
 
 const AddProjects = () => {
   return (
-    <div>
-      <h1>Add Projects</h1>
+    <div className="w-full md:w-[780px] mx-auto mt-24">
+      <div className={cn("flex flex-col gap-6")}>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-center font-semibold capitalize text-2xl">
+              Add Project
+            </CardTitle>
+          </CardHeader>
+          <AddProjectForm />
+        </Card>
+      </div>
     </div>
   );
 };
---
+++ src/components/modules/Project/AddProjectForm.tsx
@@ -0,0 +1,154 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { toast } from "sonner";
+import z from "zod";
+import { Button } from "@/components/ui/button";
+import { CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/textarea";
+
+const addProjectSchema = z.object({
+  title: z.string({ error: "Title is required" }).min(10, {
+    message: "minimum 10 character long.",
+  }),
+  description: z.string({ error: "Description is required" }).min(50, {
+    message: "minimum 50 character long.",
+  }),
+  thumbnail: z.url({ error: "Must be valid url." }),
+  technologyUsed: z.string({ error: "technologyUsed is required" }).min(1, {
+    message: "minimum 1 technology required.",
+  }),
+});
+
+const AddProjectForm = () => {
+  const form = useForm<z.infer<typeof addProjectSchema>>({
+    resolver: zodResolver(addProjectSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      thumbnail: "",
+      technologyUsed: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof addProjectSchema>) {
+    try {
+      const addProjectData = {
+        title: values.title,
+        description: values.description,
+        thumbnail: values.thumbnail,
+        technologyUsed: values.technologyUsed
+          .toString()
+          .split(",")
+          .map((tag) => tag.trim()),
+      };
+      console.log(addProjectData);
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || "Login failed");
+    }
+  }
+  return (
+    <CardContent>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project Title</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="write your project title here"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="write your project description here"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="technologyUsed"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project Technology</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="write project technology used separated by comma"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid gap-3">
+              <FormField
+                control={form.control}
+                name="thumbnail"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project Image</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="give a project related image url link"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Button type="submit" className="w-full">
+                Add Project
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </CardContent>
+  );
+};
+
+export default AddProjectForm;
