3f74109f94ad1071e3b575cfc8835cd71919dbd9
mdselimme <mdselimdev@gmail.com>
2025-10-07 05:03:04 +0600

    add: manage blog table

--- src/app/(dashboard)/dashboard/manage-blog/page.tsx
+++ src/app/(dashboard)/dashboard/manage-blog/page.tsx
@@ -1,9 +1,60 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { IBlog } from "@/types/blog";
 
-const ManageBlog = () => {
+const ManageBlog = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_LINK}/blog`, {
+    cache: "no-store",
+  });
+  const { data: blogsData } = await res.json();
+
   return (
-    <div>
-      <h1>manage blog</h1>
+    <div className="container mx-auto mt-6">
+      <h3 className="text-center font-bold capitalize text-2xl mb-10">
+        Manage Blogs
+      </h3>
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-center">Blog Id</TableHead>
+            <TableHead className="text-center">Blog Title</TableHead>
+            <TableHead className="text-center">Blog Views</TableHead>
+            <TableHead className="text-center">Blog Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {blogsData === null ? (
+            <TableRow>
+              <TableCell className="font-medium">
+                No blog published yet
+              </TableCell>
+            </TableRow>
+          ) : (
+            blogsData?.map((blog: IBlog) => (
+              <TableRow key={blog?._id} className="text-center">
+                <TableCell>{blog?._id}</TableCell>
+                <TableCell>{blog?.title}</TableCell>
+                <TableCell>{blog?.views}</TableCell>
+                <TableCell>
+                  <Button className="sm:inline-flex rounded-full mr-4">
+                    Edit Blog
+                  </Button>
+                  <Button className="sm:inline-flex rounded-full">
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
     </div>
   );
 };
